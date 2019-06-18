e8dd95a6713048d42e3808785d89ec50a8468243
root <hansrajrami5@gmail.com>
2019-06-18 22:48:55 +0530

    .

---
+++ git_push.sh
@@ -0,0 +1,7 @@
+echo "# snippets" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/hansrajrami/snippets.git
+git push -u origin master
+
